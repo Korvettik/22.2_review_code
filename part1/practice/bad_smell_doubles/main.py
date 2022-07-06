047e7e8fb79aec7a5a632fd60475af818f3d374d
Ilya <korvet1988@yandex.ru>
2022-07-06 21:38:19 +0300

    06.07.2022

---
+++ part1/practice/bad_smell_doubles/main.py
@@ -0,0 +1,31 @@
+# Как то вечером три разработчика написали 
+# три метода класса `SomeClass`, каждый под себя. 
+# Методы по сути своей почти одинаковые.
+#
+# Напишите свой метод `sorted_func`, 
+# учитывая особенности всех представленных методов
+
+
+# class SomeClass:
+#     def __init__(self):
+#         self.lst = [3, 2, 1, 4, 2, 1]
+#
+#     def sorted(self):
+#         self.lst.sort()
+#         return self.lst
+#
+#     def sorting(self):
+#         return sorted(self.lst)
+#
+#     def asc_sorting(self):
+#         return sorted(self.lst, reverse=False)
+
+
+
+class SomeClass:
+    def __init__(self, reverse=True):
+        self.lst = [3, 2, 1, 4, 2, 1]
+        self.reverse = reverse
+
+    def to_sort(self):
+        return sorted(self.lst, reverse=self.reverse)
